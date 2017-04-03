1c48afe4468484c1d02879206c43c216b922277a
Engel Alipio <BERTO@macbook-pro-3.local>
2017-04-03 11:08:51 -0400

    Initial Push

---
+++ Bamboo Breakout/GameViewController.swift
@@ -0,0 +1,57 @@
+//
+//  GameViewController.swift
+//  Bamboo Breakout
+//
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let scene = GameScene(fileNamed:"GameScene") {
+            // Configure the view.
+            
+            self.navigationController?.title = "Changed"
+            let skView = self.view as! SKView
+            skView.showsFPS = false
+            skView.showsNodeCount = false
+            
+            /* Sprite Kit applies additional optimizations to improve rendering performance */
+            skView.ignoresSiblingOrder = true
+            
+            /* Set the scale mode to scale to fit the window */
+            scene.scaleMode = .aspectFit
+            
+            skView.presentScene(scene)
+        }
+    }
+
+    override var shouldAutorotate : Bool {
+        return false
+    }
+
+    override var supportedInterfaceOrientations : UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .portrait
+        } else {
+            return .portrait
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+    override var prefersStatusBarHidden : Bool {
+       var hideBar = true as Bool
+        if UIDevice.current.userInterfaceIdiom == .pad {
+           hideBar = false
+        }
+        return hideBar
+    }
+}
